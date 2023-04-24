354f76dcfc75b47aa314fc3a5c569adeb0d6802d
lashagelashvili <las.gelashvili@gmail.com>
2023-04-24 11:23:55 +0400

    add jwt

---
+++ helpers/jwt.js
@@ -0,0 +1,27 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+function authJwt() {
+  const secret = process.env.SECRET;
+  return jwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["POST", "OPTIONS"] },
+    ],
+  });
+}
+
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
+
+  done();
+}
+
+module.exports = authJwt;
