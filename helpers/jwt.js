9be6974445c4ddf5fba0802809cbb2a99a8db814
lashagelashvili <las.gelashvili@gmail.com>
2023-05-21 16:43:18 +0400

    disable authentication

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -8,10 +8,11 @@ function authJwt() {
     isRevoked: isRevoked,
   }).unless({
     path: [
-      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/api\/v1\/orders(.*)/, methods: ["POST", "OPTIONS"] },
+      // { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      // { url: /\/api\/v1\/orders(.*)/, methods: ["POST", "OPTIONS"] },
+      { path: /(.*)/ },
     ],
   });
 }
