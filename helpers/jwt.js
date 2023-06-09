4991c14820f47dbb8ff228170df8e6a24a2f7722
lashagelashvili <las.gelashvili@gmail.com>
2023-06-09 23:45:05 +0400

    cleanup

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,27 +2,31 @@ const { expressjwt: jwt } = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.SECRET;
+  const api = process.env.API_URL;
+
   return jwt({
     secret,
     algorithms: ["HS256"],
-    isRevoked: isRevoked,
+    // isRevoked: isRevokedCallback,
   }).unless({
     path: [
-      // { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
-      // { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
-      // { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
-      // { url: /\/api\/v1\/orders(.*)/, methods: ["POST", "OPTIONS"] },
-      { path: /(.*)/ },
+      { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      `${api}/users/login`,
+      `${api}/users/register`,
     ],
   });
 }
 
-async function isRevoked(req, payload, done) {
-  if (!payload.isAdmin) {
-    done(null, true);
-  }
-
-  done();
-}
+const isRevokedCallback = async (req, token) => {
+  console.log(req, "kkkkk");
+  console.log(token, "token;;;;;;");
+  // const issuer = token.payload.iss;
+  // const tokenId = token.payload.jti;
+  // const token2 = await data.getRevokedToken(issuer, tokenId);
+  // return token2 !== "undefined";
+};
 
 module.exports = authJwt;
